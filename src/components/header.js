54c905efa552105bc8d8904d4adf1a72be99ced6
KJ597 <Jaswalkuljot@gmail.com>
2026-04-11 19:44:15 -0600

    Finsihed the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
 
 function Header() {
+    const userId = localStorage.getItem("userId");
+    const handleLogout = () => {
+        localStorage.removeItem("userId");
+        localStorage.removeItem("username");
+        window.location.href = "/";
+    };
   return (
     <>
       <header>
         <img src="/images/logo.webp" alt="Sweet Scoop" />
         <h1>Sweet Scoop Ice Cream Shop</h1>
+        {userId ? ( <button onClick={handleLogout}>Logout</button>) : (<Link to="/login">Login</Link>
+        )}
       </header>
 
       <div className="navbar">
         <Link to="/">Home</Link>
         <Link to="/flavors">Flavors</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/orderHistory">Order History</Link>
       </div>
     </>
   );
